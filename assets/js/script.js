ea37eb145ec06e23616920ac95394f78973e086e
Y39WebDeveloper <y39freelancer@gmail.com>
2022-12-19 18:42:23 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,18 @@ $(function () {
     $(document).ready(function(){
         $('.level-slider').slick({
             dots: true,
-            
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
         });
     });
 });
