af4d329d483207afa5b5e1077494739ef0bbf0d7
Y39WebDeveloper <y39freelancer@gmail.com>
2022-12-19 17:43:25 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 $(function () {
     $("#inputCheckIn").datepicker();
     $("#inputCheckOut").datepicker();
+
+    $(document).ready(function(){
+        $('.level-slider').slick({
+            dots: true,
+            
+        });
+    });
 });
