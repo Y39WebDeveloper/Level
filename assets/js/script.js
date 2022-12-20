de432a2877fcdd07882d436a0ad07203a280a824
Y39WebDeveloper <y39freelancer@gmail.com>
2022-12-20 19:11:25 +0100

    ..

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,4 +19,21 @@ $(function () {
             ]
         });
     });
+
+    function videoToggle(){
+        video = $('.level-video').get(0);
+        if(video.paused){
+            video.play();
+            $('.video-control-play').hide();
+            $('.video-control-pause').show();
+        }else{
+            video.pause();
+            $('.video-control-pause').hide();
+            $('.video-control-play').show();
+        }
+    }
+
+    $('.video-control-play, .video-control-pause').click(function(){
+        videoToggle();
+    });
 });
